59cf531761148c8700b1c590698247029e9d7e92
dhialfa <dhadiego@gmail.com>
2025-09-02 20:33:21 -0600

    requirements

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,9 +74,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("MYSQL_DATABASE", "hidalgo_DB"),
+        "USER": os.getenv("MYSQL_USER", "root"),
+        "PASSWORD": os.getenv("MYSQL_PASSWORD", "admin123"),
+        "HOST": os.getenv("MYSQL_HOST", "127.0.0.1"),
+        "PORT": int(os.getenv("MYSQL_PORT", "3306")),
+        "OPTIONS": {
+            # Charset recomendado para emojis y caracteres latinos
+            "charset": "utf8mb4",
+            "use_unicode": True,
+            # Asegura JSONField y comportamiento estricto
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES', time_zone='+00:00'",
+        },
     }
 }
 
