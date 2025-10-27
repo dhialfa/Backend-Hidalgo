b34fec10c07c346d2f73a3f6c7b2439da091bead
dhialfa <dhadiego@gmail.com>
2025-10-27 10:49:54 -0600

    URLs DEBUG for Media

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,8 @@ from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.views import EmailTokenObtainPairView
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(title="Backend Hidalgo API", default_version="v1"),
@@ -30,3 +32,8 @@ urlpatterns = [
     path("api/swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("api/redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
+
+
+# Solo en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
