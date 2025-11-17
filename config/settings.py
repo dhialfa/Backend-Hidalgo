51bba688a7a76635cd84ef5551764f90d48d539d
dhialfa <dhadiego@gmail.com>
2025-11-17 16:18:58 -0600

    Fix: settings con templates y config de email

--- config/settings.py
+++ config/settings.py
@@ -161,8 +161,6 @@ AUTHENTICATION_BACKENDS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-<<<<<<< Updated upstream
-=======
 
 
 # Send email after complete a visit...
@@ -178,4 +176,4 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
->>>>>>> Stashed changes
+
